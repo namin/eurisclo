4da16f618247741dd5323b244671d5b90f706222
Nada Amin <namin@seas.harvard.edu>
2024-07-30 23:07:06 +0200

    need this, maybe for circularity

--- src/units.lisp
+++ src/units.lisp
@@ -1111,7 +1111,7 @@
   domain (o-set o-set)
   range (o-set)
   elim-slots (applics)
-  fast-alg o-set-intersect
+  fast-alg #'o-set-intersect
   recursive-alg (lambda (s1 s2)
                   (cond ((null s1) nil)
                         ((member (car s1) s2)
